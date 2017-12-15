5b4ca40481b6eedf86df6dfa203b2c9710b25dff
zonildor <33549871+zonildor@users.noreply.github.com>
2017-12-15 19:41:13 +0300

    Add files via upload

---
+++ exercise10_2b.js
@@ -0,0 +1,22 @@
+// graph from Exercise 10.2 2b (3rd edition)
+// input user property fields for each vertex as defined in the Vertex object below
+
+var _v2 = [
+	{ label: "1" }, // index = 0
+	{ label: "2" }, // index = 1
+	{ label: "3" }, // index = 2
+	{ label: "4" }, // index = 3
+	{ label: "5" }, // index = 4
+	{ label: "6" } // index = 5	
+];
+
+var _e2 = [
+	{ u: 0, v: 1, w: 2 },
+	{ u: 0, v: 2, w: 7 },
+	{ u: 1, v: 3, w: 3 },
+	{ u: 1, v: 4, w: 4 },
+	{ u: 2, v: 3, w: 2 },
+	{ u: 2, v: 4, w: 2 },
+	{ u: 3, v: 5, w: 1 },
+	{ u: 4, v: 5, w: 5 }
+];
